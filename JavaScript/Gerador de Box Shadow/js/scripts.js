b4bd71fd171a9c3d0a40f595f2de6b84488297e0
Rodrigo Silva <rodrigodev195@gmail.com>
2025-02-26 12:34:41 -0300

    gerando box-shadow

--- JavaScript/Gerador de Box Shadow/js/scripts.js
+++ JavaScript/Gerador de Box Shadow/js/scripts.js
@@ -47,6 +47,25 @@ class BoxShadowGenerator {
     this.webkitRule.innerText = this.currentRule;
     this.mozRule.innerText = this.currentRule;
   }
+
+  updateValue(type, value) {
+    switch (type) {
+      case "horizontal":
+        this.horizontalRef.value = value;
+        break;
+      case "vertical":
+        this.verticalRef.value = value;
+        break;
+      case "blur":
+        this.blurRef.value = value;
+        break;
+      case "spread":
+        this.spreadRef.value = value;
+        break;
+    }
+    this.applyRule();
+    this.showRule();
+  }
 }
 // Seleção de Elementos
 const horizontal = document.querySelector("#horizontal");
@@ -82,3 +101,26 @@ const boxShadow = new BoxShadowGenerator(
 boxShadow.initialize();
 
 // Eventos
+horizontal.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  boxShadow.updateValue("horizontal", value);
+});
+
+vertical.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  boxShadow.updateValue("vertical", value);
+});
+
+blur.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  boxShadow.updateValue("blur", value);
+});
+
+spread.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  boxShadow.updateValue("spread", value);
+});
