f306f9e2a5af5d89ccc86a5c4cfe3340c813b661
Rodrigo Silva <rodrigodev195@gmail.com>
2025-03-04 15:41:23 -0300

    hierarquia de componentes

--- React/2_Fundamentos/fundamentos/src/App.jsx
+++ React/2_Fundamentos/fundamentos/src/App.jsx
@@ -2,9 +2,12 @@ import "./App.css";
 
 // 2 - importando componente
 import FirstComponent from "./components/FirstComponent";
+import MyComponent from "./components/MyComponent";
 
 // 4 - template expression
 import TemplateExpression from "./components/TemplateExpression";
+
+// 5 - hierarquia de componentes
 
 function App() {
   return (
@@ -13,8 +16,8 @@ function App() {
       {/* 3 - comentario JSX */}
       <h1>Fundamentos do React</h1>
       <FirstComponent />
-
       <TemplateExpression />
+      <MyComponent />
     </>
   );
 }
--- React/2_Fundamentos/fundamentos/src/components/FirstComponent.jsx
+++ React/2_Fundamentos/fundamentos/src/components/FirstComponent.jsx
@@ -1,9 +1,13 @@
+// 5 - hierarquia de componentes
+import MyComponent from "./MyComponent";
+
 // 1 - Criando componentes
 
 const FirstComponent = () => {
   return (
     <div>
       <h2>Meu primeiro componente</h2>
+      <MyComponent />
     </div>
   );
 };
