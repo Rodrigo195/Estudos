61e5cfc2dbe7aca83d81a0b44b029eb66e54975f
Rodrigo Silva <rodrigodev195@gmail.com>
2025-03-04 15:18:36 -0300

    Criando Componente

---
+++ React/2_Fundamentos/fundamentos/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import FirstComponent from "./components/FirstComponent";
+
+function App() {
+  return (
+    <>
+      <h1>Fundamentos do React</h1>
+      <FirstComponent />
+    </>
+  );
+}
+
+export default App;
