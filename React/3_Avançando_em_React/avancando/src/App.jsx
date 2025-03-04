0a0c75fdb1f106842339f51cf84d41552e506a17
Rodrigo Silva <rodrigodev195@gmail.com>
2025-03-04 17:42:38 -0300

    useState

--- React/3_Avançando_em_React/avancando/src/App.jsx
+++ React/3_Avançando_em_React/avancando/src/App.jsx
@@ -3,15 +3,20 @@ import "./App.css";
 // 2 - imagem em assets
 import night from "./assets/night.jpg";
 
+// 3 - useState
+import Data from "./components/Data.jsx";
+
 function App() {
   return (
-    <>
+    <div className="App">
       <h1>Avançando em React</h1>
       {/* 1 - imagem em public */}
       <img src="/img.jpg" alt="natureza" />
       {/* 2 - imagem em assets */}
       <img src={night} alt="noite" />
-    </>
+      {/* 3 - useState */}
+      <Data />
+    </div>
   );
 }
 
